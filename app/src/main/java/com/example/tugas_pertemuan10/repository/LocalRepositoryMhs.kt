e1b6877dce468fe950b4b3ad77129cb2695a9ec7
defarrel <Defarrel>
2024-12-14 09:21:00 +0700

    Menambahkan Read, Delete, dan Update di MahasiswaDao

--- app/src/main/java/com/example/tugas_pertemuan10/repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/example/tugas_pertemuan10/repository/LocalRepositoryMhs.kt
@@ -2,9 +2,12 @@ package com.example.tugas_pertemuan10.repository
 
 import com.example.tugas_pertemuan10.data.dao.MahasiswaDao
 import com.example.tugas_pertemuan10.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 class LocalRepositoryMhs (private val mahasiswaDao: MahasiswaDao): RepositoryMhs {
     override suspend fun insertMhs(mahasiswa: Mahasiswa) {
         mahasiswaDao.insertMahasiswa(mahasiswa)
     }
+
+
 }
--- app/src/main/java/com/example/tugas_pertemuan10/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/tugas_pertemuan10/repository/RepositoryMhs.kt
@@ -1,7 +1,9 @@
 package com.example.tugas_pertemuan10.repository
 
 import com.example.tugas_pertemuan10.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+
 }
