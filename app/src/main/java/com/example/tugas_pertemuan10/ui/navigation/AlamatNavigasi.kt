7113e3ab67288071368d152ebda1e7bb5534bd04
defarrel <Defarrel>
2024-12-14 09:28:13 +0700

    Membuat object di AlamatNavigasi untuk menandai view

--- app/src/main/java/com/example/tugas_pertemuan10/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/tugas_pertemuan10/ui/navigation/AlamatNavigasi.kt
@@ -2,4 +2,20 @@ package com.example.tugas_pertemuan10.ui.navigation
 
 interface AlamatNavigasi {
     val route: String
+
+    object DestinationInsert : AlamatNavigasi{
+        override val route = "home"
+    }
+
+    object DestinationDetail : AlamatNavigasi{
+        override val route =  "detail"
+        const val NIM = "nim"
+        val routeWithArgs = "$route/{$NIM}"
+    }
+
+    object DestinationUpdate : AlamatNavigasi{
+        override val route =  "update"
+        const val NIM = "nim"
+        val routeWithArgs = "$route/{$NIM}"
+    }
 }
