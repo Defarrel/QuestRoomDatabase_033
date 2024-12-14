ef2bd46992397cf89435c7aef6e00fedcffb988a
defarrel <Defarrel>
2024-12-14 09:23:49 +0700

    Menambahkan fungsi2 tadi di LocalRepositoryMhs untuk menghubungkan Mahasiswadao dengan operasi yang didefinisikan dalam repostoryMhs

--- app/src/main/java/com/example/tugas_pertemuan10/repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/example/tugas_pertemuan10/repository/LocalRepositoryMhs.kt
@@ -9,5 +9,19 @@ class LocalRepositoryMhs (private val mahasiswaDao: MahasiswaDao): RepositoryMhs
         mahasiswaDao.insertMahasiswa(mahasiswa)
     }
 
+    override fun getAllMhs(): Flow<List<Mahasiswa>> {
+        return mahasiswaDao.getAllMahasiswa()
+    }
 
+    override fun getMhs(nim: String): Flow<Mahasiswa> {
+        return mahasiswaDao.getMahasiswa(nim)
+    }
+
+    override suspend fun deleteMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.deleteMahasiswa(mahasiswa)
+    }
+
+    override suspend fun updateMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.updateMahasiswa(mahasiswa)
+    }
 }
